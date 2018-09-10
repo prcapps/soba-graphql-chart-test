88c19aca5e87374bd454a81dd9e197f5124b0368
Patrick Conant <conantp@prcappss-mbp.lan>
2018-09-10 10:11:58 -0400

    Starting work on monday

--- react/soba-visualization-component/src/SobaTable.js
+++ react/soba-visualization-component/src/SobaTable.js
@@ -1,81 +1,6 @@
 import React, { Component } from 'react';
 import '../css/SobaTable.css';
-
-class SobaRow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { expanded: props.expanded };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    console.log('update', name, value);
-    this.setState({
-      [name]: value,
-    });
-  }
-
-  render() {
-    const title = this.props.item.grouptitle;
-    const count = this.props.item.count;
-    const subitems = this.props.item.subitems;
-
-    let className = '';
-
-    if (this.state.expanded !== true) {
-      className = 'subTable';
-    }
-
-    let subItemExpand = (
-      <input
-        name="expanded"
-        type="checkbox"
-        value={this.state.expanded}
-        onChange={this.handleInputChange}
-      />
-    );
-
-    let showSubItems = false;
-    if (subitems && subitems.length && subitems[0].groupcategory) {
-      showSubItems = true;
-    }
-    if(count == false){
-      className = '';
-      subItemExpand = '';
-      subitems[0].groupcategory = '';
-    }
-
-    let subitemsElm;
-    if (showSubItems) {
-      subitemsElm = (
-        <div className={className}>
-          <strong>{ subitems[0].groupcategory }</strong>
-          <SobaTable items={subitems} />
-        </div>
-      );
-    } else {
-      subItemExpand = false;
-    }
-
-    
-
-    return (
-      <tr>
-        <td>{ title }</td>
-        <td>
-          { count }
-          { subItemExpand }
-          { subitemsElm }
-        </td>
-      </tr>
-    );
-  }
-}
+import SobaRow from './SobaRow';
 
 class SobaTable extends Component {
   constructor(props) {
@@ -89,7 +14,7 @@ class SobaTable extends Component {
         <table>
           <tbody>
             {this.props.items.map(item => (
-              <SobaRow item={item} expanded="false" />
+              <SobaRow item={item} key={item.grouptitle} expanded="false" />
             ))}
           </tbody>
         </table>
--- react/soba-visualization-component/src/SobaVisualization.js
+++ react/soba-visualization-component/src/SobaVisualization.js
@@ -8,6 +8,12 @@ import {
   Tab, Tabs, TabList, TabPanel,
 } from 'react-tabs';
 
+
+const API_KEY = 'AIzaSyC7rROXbT3W8IP4gs0oMtDGxumkMF4CFXo';
+
+
+// import { drive_v3 } from 'googleapis';
+// import { GoogleSheets } from 'google-drive-sheets';
 // import { GoogleSpreadsheets } from 'google-spreadsheets';
 
 
@@ -72,6 +78,7 @@ class SobaVisualization extends Component {
       dataMode: 'graphql',
       spreadsheetID: false,
       showChartTypeSelect: true,
+      gapiReady: false,
     };
 
     const {
@@ -98,8 +105,31 @@ class SobaVisualization extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  // PRC - Clean this up! 
+  loadGoogleAPI() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    const obj = this;
+
+    script.onload = () => {
+      gapi.load('client', () => {
+        gapi.client.setApiKey(API_KEY);
+        gapi.client.load('sheets', 'v4', () => {
+          // console.log("GOOGLE LOADED", this, this.state.gapiReady, this.state.dataMode);
+
+          obj.getData();
+          obj.setState({ gapiReady: true });
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
+
   componentDidMount() {
     this.getData();
+    this.loadGoogleAPI();
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -119,49 +149,85 @@ class SobaVisualization extends Component {
     const { dataMode } = this.state;
 
     if (dataMode === 'google') {
+      if (typeof gapi === typeof undefined) {
+        console.log('not ready');
+        return;
+      }
+
+      // console.log('gapi', gapi);
+
       const {  spreadsheetID } = this.state;
       // ID of the Google Spreadsheet
       // const spreadsheetID = '1oyMRNYXrTCBLXh0166zN432k7Pg7TIvu5FrEa8znjQ4';
       const url = 'https://spreadsheets.google.com/feeds/list/' + spreadsheetID + '/od6/public/values?alt=json';
 
-      // const opts = { key: spreadsheetID, worksheet: 1 };
-      // console.log(GoogleSpreadsheets);
-      // GoogleSpreadsheets.rows(opts).then((data) => {
-      //   console.log('patrick', data);
+      const params = {
+        // The ID of the spreadsheet to retrieve data from.
+        spreadsheetId: spreadsheetID,  // TODO: Update placeholder value.
 
-      // });
+        // The A1 notation of the values to retrieve.
+        range: 'A:Z',  // TODO: Update placeholder value.
+      };
+
+      var request = gapi.client.sheets.spreadsheets.values.get(params);
+      request.then((response) => {
+        // TODO: Change code below to process the `response` object:
+        console.log('data coming back', response.result);
+
+        if (response.result) {
+          const chartData = this.processGoogleSheetForChartFromAPI(response.result.values);
+          this.setState({
+            chartDatasets: chartData[0],
+            chartLabels: chartData[1],
+          });
+          this.setState({
+            items: chartData[2],
+            errors: false,
+            loadingChart: false,
+          });
+        } else {
+          this.setState({
+            errors: 'Google Sheet Error',
+            loadingChart: false,
+            items: [],
+          });
+        }
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
 
 
       // eslint-disable-next-line
-      fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          // eslint-disable-next-line
-          'Accept': 'application/json',
-        },
-      })
-        .then(r => r.json())
-        .then((data) => {
-          if (data.feed.entry) {
-            const chartData = this.processGoogleSheetForChart(data.feed.entry);
-            this.setState({
-              chartDatasets: chartData[0],
-              chartLabels: chartData[1],
-            });
-            this.setState({
-              items: chartData[2],
-              errors: false,
-              loadingChart: false,
-            });
-          } else {
-            this.setState({
-              errors: 'Google Sheet Error',
-              loadingChart: false,
-              items: [],
-            });
-          }
-        });
+      // fetch(url, {
+      //   method: 'GET',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     // eslint-disable-next-line
+      //     'Accept': 'application/json',
+      //   },
+      // })
+      //   .then(r => r.json())
+      //   .then((data) => {
+      //     console.log('test123', data);
+      //     if (data.feed.entry) {
+      //       const chartData = this.processGoogleSheetForChart(data.feed.entry);
+      //       this.setState({
+      //         chartDatasets: chartData[0],
+      //         chartLabels: chartData[1],
+      //       });
+      //       this.setState({
+      //         items: chartData[2],
+      //         errors: false,
+      //         loadingChart: false,
+      //       });
+      //     } else {
+      //       this.setState({
+      //         errors: 'Google Sheet Error',
+      //         loadingChart: false,
+      //         items: [],
+      //       });
+      //     }
+      //   });
 
       // $('.results').prepend('<h2>'+this.gsx$name.$t+'</h2><p>'+this.gsx$age.$t+'</p>');
 
@@ -215,7 +281,7 @@ class SobaVisualization extends Component {
         dataset, byDate, groupBy, count, dateField, filters,
       };
 
-      console.log(inputVariables);
+      // console.log(inputVariables);
       const bodyStringArgs = {
         query,
         variables: inputVariables,
@@ -277,10 +343,67 @@ class SobaVisualization extends Component {
     });
   }
 
+  processGoogleSheetForChartFromAPI(data) {
+    console.log('data', data);
+
+    const chartLabels = [];
+    const chartDatasets = [];
+
+    // First row is headers
+    const firstRow = data.shift(); 
+
+    const yearLabel = firstRow.shift();
+    firstRow.forEach((value) => {
+      const dataset = {};
+      dataset.label = value;
+      dataset.coordinates = [];
+      dataset.data = [];
+      dataset.backgroundColor = chartColors[chartDatasets.length];
+
+      chartDatasets.push(dataset);
+    });
+
+    console.log(firstRow);
+    console.log(chartDatasets);
+
+    console.log(this);
+    const tableData = [];
+
+    data.forEach((value_array, columnIndex) => {
+      const tableRow = {};
+
+      const yearTitle = value_array.shift();
+
+      console.log('data set', yearTitle);
+      chartLabels.push(yearTitle);
+      tableRow.grouptitle = yearTitle;
+      tableRow.groupcategory = yearLabel;
+      tableRow.count = '';
+      tableRow.subitems = [];
+
+      value_array.forEach((value, dataSetIndex) => {
+        chartDatasets[dataSetIndex].data[columnIndex] = value;
+
+        tableRow.subitems[dataSetIndex] = {
+          grouptitle: chartDatasets[dataSetIndex].label, groupcategory: 'category', count: value, subitems: [],
+        };
+      });
+
+      tableData.push(tableRow);
+    });
+
+    // console.log('labels', chartLabels);
+    // console.log('data', chartDatasets);
+    // console.log('table', tableData);
+
+    return [chartDatasets, chartLabels, tableData];
+  }
+
   processGoogleSheetForChart(data) {
+    console.log('data', data);
     const chartLabels = [];
 
-    // console.log(this);
+    console.log(this);
 
     const chartDatasets = [];
     const chartDatasetsLookup = {}; // subitem_label => idnex
@@ -354,14 +477,11 @@ class SobaVisualization extends Component {
   }
 
   processGraphQLForChart(data) {
-    // console.log('result', data, this);
-
     const chartLabels = [];
 
     data.forEach((row) => {
       chartLabels.push(row.grouptitle);
     });
-    console.log('labels', chartLabels);
 
     const chartDatasets = [];
     const chartDatasetsLookup = {}; // subitem_label => idnex
@@ -489,9 +609,10 @@ class SobaVisualization extends Component {
 
     let settings = false;
     let settingsTab = false;
+    let settingsPanel = false;
     if (dataMode === 'graphql') {
       settingsTab = <Tab>Settings</Tab>;
-
+      settingsPanel = <TabPanel>{settings}</TabPanel>;
       settings = (
         <div className="inputs">
           <div>
@@ -572,9 +693,7 @@ class SobaVisualization extends Component {
           <TabPanel>
             { table }
           </TabPanel>
-          <TabPanel>
-            {settings}
-          </TabPanel>
+          {settingsPanel}
           <TabList>
             <Tab>
               Chart
