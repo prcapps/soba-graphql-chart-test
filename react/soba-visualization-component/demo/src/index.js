d01b790eafbdf24b672241f5e191b3b5f258898a
Patrick Conant <conantp@prcappss-mbp.lan>
2018-09-12 14:48:45 -0400

    Publishing 1.7.0

---
+++ react/soba-visualization-component/demo/src/index.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+// import {render} from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import Example from '../../src'
+
+class Demo extends Component {
+
+
+  render() {
+  	const filterObj = {
+		op: "AND", 
+		groups:
+			[
+				{
+					op: "AND", 
+					filters: 
+					[
+						{key: "search_initiated", op: "=", value: "1"}, 
+						{key: "EXTRACT(year FROM date_occurred)", op: "=", value: "2018"}, 
+					]
+				},
+				{
+					op: "OR", 
+					filters: 
+					[
+						{key: "name_race", op: "=", value: "B"}, 
+						{key: "name_race", op: "=", value: "W"}
+					]
+				}
+			]
+	};
+    return <div>
+      <h1>soba-visualization Demo</h1>
+      
+
+      <Example 
+      	title='APD Traffic Searches by Race, 2018'
+		chartType='line'  
+		dataset='coa_apd_traffic_stop_name_data_table' 
+		count='traffic_stop_id' 
+		byDate='["month"]' 
+		groupBy='["name_race"]'
+		filters={filterObj}
+		/>
+	    <Example 
+      	title='patrickdemo 3'
+	    spreadsheetId='1ZZbKNL4bxISIiAeUfsYZ-7ajfZPQ8VZ955WY0XP6ioE'
+	    chartType='bar'  
+	    showChartTypeSelect='0'
+	    />
+
+    </div>
+  }
+}
+
+
+const domContainer = document.querySelector('#demo');
+ReactDOM.render(React.createElement(Demo), domContainer);
+
+// render(<Demo/>, document.querySelector('#demo'))
---
+++ react/soba-visualization-component/src/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+import SobaVisualization from './SobaVisualization';
+
+
+export default class extends Component {
+  render() {
+    return <div>
+       <SobaVisualization 
+      	title={this.props.title}
+  			chartType={this.props.chartType}
+  			dataset={this.props.dataset}
+  			count={this.props.count}
+  			byDate={this.props.byDate}
+  			groupBy={this.props.groupBy}
+        filters={this.props.filters}
+        spreadsheetId={this.props.spreadsheetId}
+        showChartTypeSelect={this.props.showChartTypeSelect}
+  			/>
+    </div>
+  }
+}
+
+// export * from './SobaVisualization';
